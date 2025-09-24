437bb520af9fc643140a99b27cd7426c97c0073b
Степан Булычев <bulychev.stepan2000@gmail.com>
2025-09-24 20:58:49 +0300

    Пример вызова js функций из подключаемой библиотеки

---
+++ CommonComponents/Models/ViewportSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonComponents.Models
+{
+    public class ViewportSize
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+    public class DeviceInfo
+    {
+        public bool IsMobile { get; set; }
+        public bool IsTablet { get; set; }
+        public bool IsDesktop { get; set; }
+        public string UserAgent { get; set; }
+        public int ScreenWidth { get; set; }
+        public int ScreenHeight { get; set; }
+    }
+}
---
+++ CommonComponents/wwwroot/deviceDetector.js
@@ -0,0 +1,20 @@
+﻿export function getDeviceInfo() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+    const isTablet = /ipad|android(?=.*mobile)/i.test(userAgent);
+
+    return {
+        isMobile: isMobile,
+        isTablet: isTablet,
+        isDesktop: !isMobile && !isTablet,
+        userAgent: navigator.userAgent,
+        screenWidth: window.screen.width,
+        screenHeight: window.screen.height
+    };
+}
+export function getViewportSize() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+}
